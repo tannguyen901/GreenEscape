3f85699bd2af6df26d42f9f6d96f830ccf972511
unknown <tannguyen901@gmail.com>
2023-09-21 13:26:42 -0700

    authorization for individual users, delete/edit campground and reviews

--- seeds/index.js
+++ seeds/index.js
@@ -33,6 +33,7 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      author: "650b8e553e8c8ddbb75953b4",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       image: "https://source.unsplash.com/collection/483251",
